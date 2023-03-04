2ad0e579a10bd3865b68ed7a2c9db12c5ff5a721
Carlos <carloshtbraga@gmail.com>
2023-03-04 16:21:47 -0300

    Footer
    Monta o footer,coloca nas pages,  coloca position fixed, bottom 0, e faz os botoes levarem para as rotas correspondentes

--- src/pages/meals/Meals.jsx
+++ src/pages/meals/Meals.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Header } from '../../components';
+import Footer from '../../components/footer/Footer';
 
 function Meals() {
   return (
     <div>
       <Header hTitle="Meals" />
+      <Footer />
     </div>
   );
 }
