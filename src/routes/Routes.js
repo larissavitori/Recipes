1b6f2e7c22db5a7cdc5a916b86c8a17a7741ca57
TulioKaaz <tuliocaz.windcoast@gmail.com>
2023-03-03 21:58:37 -0300

    Create: rotes structure

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import { Login, Meals } from '../pages';
+import {
+  Login, Meals, MealView, Drinks, DrinkView, Favorites, Profile, DoneRecipes,
+} from '../pages';
 
 function Routes() {
   return (
     <Switch>
       <Route exact path="/" component={ Login } />
       <Route exact path="/meals" component={ Meals } />
+      <Route
+        exact
+        path="/meals/:id"
+        render={ (props) => <MealView { ...props } /> }
+      />
+      <Route exact path="/drinks" component={ Drinks } />
+      <Route
+        exact
+        path="/drinks/:id"
+        render={ (props) => <DrinkView { ...props } /> }
+      />
+      <Route exact path="/profile" component={ Profile } />
+      <Route exact path="/done-recipes" component={ DoneRecipes } />
+      <Route exact path="/favorite-recipes" component={ Favorites } />
     </Switch>
   );
 }
