7908e00550d51957e134e85e4912697260731201
TulioKaaz <tuliocaz.windcoast@gmail.com>
2023-03-04 23:43:56 -0300

    Refactor: Buttons

--- src/components/buttons/IconButton.jsx
+++ src/components/buttons/IconButton.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import './iconButton.css';
+import './buttons.css';
 
 function IconButton({ hDataTestId, hIconSrc, hAltText, hOnClick }) {
   return (
