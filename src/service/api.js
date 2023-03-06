8e55f372738564ff75c1be1edfa6dca566d4736c
TulioKaaz <tuliocaz.windcoast@gmail.com>
2023-03-06 18:31:43 -0300

    fix: api URLs

--- src/service/api.js
+++ src/service/api.js
@@ -1,25 +1,19 @@
 export const getDrinksByIngredient = async (ingredient) => {
-  const URL = `
-    https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${ingredient}
-  `;
+  const URL = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${ingredient}`;
   const { drinks } = await (await fetch(URL)).json();
 
   return drinks;
 };
 
 export const getDrinksByName = async (name) => {
-  const URL = `
-    https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${name}
-  `;
+  const URL = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${name}`;
   const { drinks } = await (await fetch(URL)).json();
 
   return drinks;
 };
 
 export const getDrinksByFirstLetter = async (firstLetter) => {
-  const URL = `
-    https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${firstLetter}
-  `;
+  const URL = `https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${firstLetter}`;
   const { drinks } = await (await fetch(URL)).json();
 
   return drinks;
@@ -33,27 +27,21 @@ export const getDrinksCategoryList = async () => {
 };
 
 export const getMealsByIngredient = async (ingredient) => {
-  const URL = `
-    www.themealdb.com/api/json/v1/1/filter.php?i=${ingredient}
-  `;
+  const URL = `https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingredient}`;
   const { meals } = await (await fetch(URL)).json();
 
   return meals;
 };
 
 export const getMealsByName = async (name) => {
-  const URL = `
-    www.themealdb.com/api/json/v1/1/search.php?s=${name}
-  `;
+  const URL = `https://www.themealdb.com/api/json/v1/1/search.php?s=${name}`;
   const { meals } = await (await fetch(URL)).json();
 
   return meals;
 };
 
 export const getMealsByFirstLetter = async (firstLetter) => {
-  const URL = `
-    www.themealdb.com/api/json/v1/1/search.php?f=${firstLetter}
-  `;
+  const URL = `https://www.themealdb.com/api/json/v1/1/search.php?f=${firstLetter}`;
   const { meals } = await (await fetch(URL)).json();
 
   return meals;
