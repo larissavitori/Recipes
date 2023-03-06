42b54d476f66cf38213a4096bcbc26d9e682df3c
TulioKaaz <tuliocaz.windcoast@gmail.com>
2023-03-06 18:32:49 -0300

    create: search recipes functionality

--- src/context/recipesContext/RecipesProvider.js
+++ src/context/recipesContext/RecipesProvider.js
@@ -1,7 +1,15 @@
 import PropTypes from 'prop-types';
 import React, { useState, useMemo } from 'react';
-
+import { useHistory } from 'react-router-dom';
 import RecipesContext from './RecipesContext';
+import {
+  getDrinksByFirstLetter,
+  getDrinksByIngredient,
+  getDrinksByName,
+  getMealsByFirstLetter,
+  getMealsByIngredient,
+  getMealsByName,
+} from '../../service/api';
 
 function RecipesProvider({ children }) {
   const [research, setResearch] = useState({
@@ -9,6 +17,8 @@ function RecipesProvider({ children }) {
     searchOption: 'byIngredient',
     dataBase: 'meals',
   });
+  const [recipes, setRecipes] = useState([]);
+  const history = useHistory();
 
   const researchHandleChange = ({ target: { name, value } }) => {
     setResearch({ ...research, [name]: value });
@@ -18,11 +28,63 @@ function RecipesProvider({ children }) {
     setResearch({ ...research, dataBase });
   };
 
+  const switchRecipesOpt = async (
+    [getByIngredient, getByName, getByFirstLetter],
+    searchOption,
+    search,
+  ) => {
+    let recipesData = recipes;
+    switch (searchOption) {
+    case 'byIngredient':
+      recipesData = await getByIngredient(search);
+      break;
+    case 'byName':
+      recipesData = await getByName(search);
+      break;
+    case 'byFirstLetter':
+      if (search.length !== 1) {
+        global.alert('Your search must have only 1 (one) character');
+        break;
+      }
+      recipesData = await getByFirstLetter(search);
+      break;
+    default:
+      break;
+    }
+    return recipesData;
+  };
+
+  const handleGetRecipes = async (e) => {
+    e.preventDefault();
+    const { search, dataBase, searchOption } = research;
+    let recipesData = recipes;
+    if (dataBase === 'meals') {
+      recipesData = await switchRecipesOpt(
+        [getMealsByIngredient, getMealsByName, getMealsByFirstLetter],
+        searchOption,
+        serch,
+      );
+    } else {
+      recipesData = await switchRecipesOpt(
+        [getDrinksByIngredient, getDrinksByName, getDrinksByFirstLetter],
+        searchOption,
+        search,
+      );
+    }
+    setRecipes(recipesData);
+    if (recipesData.length === 1) {
+      const { idMeal } = recipesData[0];
+      history.push(`${dataBase}/${idMeal}`);
+    }
+  };
+
   const recipesState = useMemo(() => ({
     research,
+    recipes,
     researchHandleChange,
     setDataBase,
-  }), [research, setDataBase]);
+    handleGetRecipes,
+  }), [research, recipes]);
 
   return (
     <RecipesContext.Provider value={ recipesState }>
