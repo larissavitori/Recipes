08d7d5554ba2f29e6d3a2287761051ea21bead6f
TulioKaaz <tuliocaz.windcoast@gmail.com>
2023-03-06 21:02:28 -0300

    Create: Recipe Grid

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,9 @@
 import LoginForm from './loginForm/LoginForm';
 import Header from './header/Header';
+import RecipesGrid from './recipesGrid/RecipesGrid';
 
 export {
   LoginForm,
   Header,
+  RecipesGrid,
 };
