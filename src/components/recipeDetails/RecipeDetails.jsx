d791bfb1e5910509b0651d1135a1a19ee003a4e1
larissa <laryterra@gmail.com>
2023-03-08 19:18:13 -0300

    problemas

--- src/components/recipeDetails/RecipeDetails.jsx
+++ src/components/recipeDetails/RecipeDetails.jsx
@@ -2,12 +2,19 @@ import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 
 import './recipesDetails.css';
+import CopyToClipboard from 'react-copy-to-clipboard';
+import shareIcon from '../../images/shareIcon.svg';
+import blackHeart from '../../images/blackHeartIcon.svg';
+import whiteHeartIcon from '../../images/whiteHeartIcon.svg';
 
 function RecipeDetails() {
   const [mealDetail, setMealDetail] = useState([]);
   const [drinkDetail, setDrinkDetail] = useState([]);
   const [recomendDrinks, setRecomendDrinks] = useState([]);
   const [recomendMeals, setRecomendMeals] = useState([]);
+  const [copied, setCopied] = useState(false);
+  const [value, setValue] = useState('');
+  const [favorited, setFavorited] = useState(false);
 
   const history = useHistory();
   const drinkOrMeal = history.location.pathname;
@@ -18,6 +25,7 @@ function RecipeDetails() {
       52771: [],
     },
   };
+
   async function fetchMeal() {
     const response = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`);
     const data = await response.json();
@@ -43,6 +51,8 @@ function RecipeDetails() {
   }
 
   useEffect(() => {
+    const url = `http://localhost:3000${drinkOrMeal}`;
+    setValue(url);
     if (drinkOrMeal.includes('drinks')) {
       fetchDrink();
       carouselMeals();
@@ -51,19 +61,6 @@ function RecipeDetails() {
       carouselDrinks();
     }
   }, []);
-
-  /*   const mealsOrDrinkIngredient = mealsOrDrink().map((mea) => {
-    const mealWithoutEmptyValues = {};
-    // const array1 = [];
-    Object.entries(mea).forEach(([key, value]) => {
-      if (key.includes('strIngredient') || key.includes('strMeasure')) {
-        mealWithoutEmptyValues[key] = value;
-        // array1.push(mealWithoutEmptyValues);
-      }
-    });
-    console.log(mealWithoutEmptyValues);
-    return mealWithoutEmptyValues;
-  }); */
 
   const mealsOrDrink = () => {
     if (drinkOrMeal.includes('drinks')) {
@@ -81,10 +78,10 @@ function RecipeDetails() {
   const mealWithoutEmptyValues = [];
   const mealsDrink = () => {
     mealsOrDrink().forEach((recipe) => {
-      const ingredients = Object.keys(recipe).filter((k) => k
-        .includes('strIngredient')).map((k) => recipe[k]);
-      const measures = Object.keys(recipe).filter((k) => k
-        .includes('strMeasure')).map((k) => recipe[k]);
+      const ingredients = Object.keys(recipe).filter((el) => el
+        .includes('strIngredient')).map((el) => recipe[el]);
+      const measures = Object.keys(recipe).filter((el) => el
+        .includes('strMeasure')).map((el) => recipe[el]);
       mealWithoutEmptyValues.push(ingredients, measures);
       return mealWithoutEmptyValues;
     });
@@ -95,6 +92,48 @@ function RecipeDetails() {
       history.push(`${drinkOrMeal}/in-progress`);
     }
     return null;
+  };
+  const newMeal = [{
+    id: mealDetail.length > 0 ? mealDetail[0].idMeal : null,
+    type: 'meal',
+    nationality: mealDetail.length > 0 ? mealDetail[0].strArea : null,
+    category: mealDetail.length > 0 ? mealDetail[0].strCategory : null,
+    alcoholicOrNot: '',
+    name: mealDetail.length > 0 ? mealDetail[0].strMeal : null,
+    image: mealDetail.length > 0 ? mealDetail[0].strMealThumb : null,
+  }];
+  const newDrink = [{
+    id: drinkDetail.length > 0 ? drinkDetail[0].idDrink : null,
+    type: 'drink',
+    nationality: '',
+    category: drinkDetail.length > 0 ? drinkDetail[0].strCategory : null,
+    alcoholicOrNot: drinkDetail.length > 0 ? drinkDetail[0].strAlcoholic : null,
+    name: drinkDetail.length > 0 ? drinkDetail[0].strDrink : null,
+    image: drinkDetail.length > 0 ? drinkDetail[0].strDrinkThumb : null,
+  }];
+  const newDrinkOrMeal = () => {
+    if (drinkOrMeal.includes('drinks')) {
+      return newDrink;
+    }
+    return newMeal;
+  };
+  const salvarNoLocalStorage = () => {
+    // pega o array armazenado no localStorage, se existir
+    const bebidasSalvas = JSON.parse(localStorage.getItem('favoriteRecipes')) || [];
+    const index = bebidasSalvas.filter((item) => item.id === newDrinkOrMeal().id);
+    if (index) {
+      const updatedItens = bebidasSalvas
+        .filter((item) => item.id !== newDrinkOrMeal().id);
+      setFavorited(false);
+      localStorage.setItem('favoriteRecipes', JSON.stringify(updatedItens));
+    } else {
+      const FiltroDrinkOrMeals = [...bebidasSalvas, newDrinkOrMeal()];
+      setFavorited(true);
+      localStorage.setItem('favoriteRecipes', JSON.stringify(FiltroDrinkOrMeals));
+    }
+  };
+  const handleClickFavorite = () => {
+    salvarNoLocalStorage();
   };
   return (
     <div>
@@ -164,14 +203,44 @@ function RecipeDetails() {
           </div>
         ))}
       </div>
-      <button
+      <CopyToClipboard
+        text={ value }
+        onCopy={ () => {
+          setCopied(true);
+        } }
+      >
+        <button
+          type="button"
+          data-testid="share-btn"
+        >
+          <img src={ shareIcon } alt="share" />
+        </button>
+
+      </CopyToClipboard>
+      {/*   <button
         type="button"
         data-testid="start-recipe-btn"
         className="startRecipe"
         onClick={ handleClick }
       >
         { !inProgressRecipes.length ? 'Continue Recipe' : 'Start Recipes' }
+      </button> */}
+      <button
+        type="button"
+        onClick={ handleClickFavorite }
+      >
+
+        <img
+          data-testid="favorite-btn"
+          src={ favorited ? blackHeart : whiteHeartIcon }
+          alt="blackHeartIcon"
+        />
+
       </button>
+      <p>
+        { copied
+          ? 'Link copied!' : null }
+      </p>
     </div>
   );
 }
