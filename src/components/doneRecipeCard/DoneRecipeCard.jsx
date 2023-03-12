c75b41e6b5b70db832f7dd34dab2c215af07e388
TulioKaaz <tuliocaz.windcoast@gmail.com>
2023-03-12 15:57:12 -0300

    Create: Done Recipes Card

---
+++ src/components/doneRecipeCard/DoneRecipeCard.jsx
@@ -0,0 +1,137 @@
+import PropTypes from 'prop-types';
+import React, { useEffect } from 'react';
+import { BiShareAlt } from 'react-icons/bi';
+import FavoriteBtn from '../buttons/FavoriteBtn';
+
+function DoneRecipeCard({ recipeData, index }) {
+  const [isFavorite, setIsFavorite] = useState(false);
+
+  const handleShare = async () => {
+    let url = window.location.href;
+    const { type, id } = recipeData;
+    const urlParms = type === 'meal' ? `meals/${id}` : `drinks/${id}`;
+
+    url = url.replace('done-recipes', urlParms);
+
+    await clipboardCopy(url);
+  };
+
+  const handleFavorite = () => {
+    const {
+      id, type, nationality, category, alcoholicOrNot, name, image,
+    } = recipeData;
+    const favoriteRecipes = JSON.parse(localStorage.getItem('favoriteRecipes')) || [];
+    const favoriteData = {
+      id,
+      type,
+      nationality,
+      category,
+      alcoholicOrNot,
+      name,
+      image,
+    };
+
+    localStorage.setItem('favoriteRecipes', JSON.stringify([
+      ...favoriteRecipes,
+      favoriteData,
+    ]));
+
+    setIsFavorite(true);
+  };
+
+  const handleUnfavorite = () => {
+    const { id } = recipeData;
+    const favoriteRecipes = JSON.parse(localStorage.getItem('favoriteRecipes')) || [];
+    const filteredFavorites = favoriteRecipes.filter(
+      (recipe) => (recipe.id !== id),
+    );
+
+    localStorage.setItem('favoriteRecipes', JSON.stringify([...filteredFavorites]));
+
+    setIsFavorite(false);
+  };
+
+  useEffect(() => {
+    const { id } = recipeData;
+    const favoriteRecipes = JSON.parse(localStorage.getItem('favoriteRecipes')) || [];
+
+    setIsFavorite(favoriteRecipes.some((recipe) => recipe.id === id));
+  }, []);
+
+  return (
+    <div className="done-recipe-card">
+      <img
+        data-testid={ `${index}-horizontal-image` }
+        className="done-recipe-image"
+        src={ recipeData.image }
+        alt={ `${recipeData.name}` }
+      />
+      <div className="detail-options">
+        <button
+          data-testid={ `${index}-horizontal-share-btn` }
+          className="option-btn"
+          onClick={ handleShare }
+        >
+          <BiShareAlt className="option-icon" />
+        </button>
+        {
+          isFavorite ? <FavoriteBtn
+            fHandleClick={ handleUnfavorite }
+            isFavorite={ isFavorite }
+          /> : <FavoriteBtn
+            fHandleClick={ handleFavorite }
+            isFavorite={ isFavorite }
+          />
+        }
+      </div>
+      <span
+        data-testid={ `${index}-horizontal-top-text` }
+        className="recipe-category"
+      >
+        {recipeData.category}
+      </span>
+      <span
+        data-testid={ `${index}-horizontal-name` }
+        className="done-recipe-name"
+      >
+        {recipeData.name}
+      </span>
+      <p
+        className="done-recipe-date"
+        data-testid={ `${index}-horizontal-done-date` }
+      >
+        {recipeData.doneDate}
+      </p>
+      <p className="recipe-tags">
+        Tags:
+        {
+          recipeData.tags.map((tag, id) => (
+            <span
+              key={ id }
+              data-testid={ `${index}-${tag}-horizontal-done-date` }
+            >
+              {tag}
+            </span>
+          ))
+        }
+      </p>
+    </div>
+  );
+}
+
+DoneRecipeCard.propTypes = {
+  index: PropTypes.number.isRequired,
+  recipeData: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    type: PropTypes.string.isRequired,
+    nationality: PropTypes.string,
+    category: PropTypes.string.isRequired,
+    alcoholicOrNot: PropTypes.string,
+    name: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+    doneDate: PropTypes.string.isRequired,
+    tags: PropTypes.arrayOf(PropTypes.string).isRequired,
+  }).isRequired,
+};
+
+export default DoneRecipeCard;
