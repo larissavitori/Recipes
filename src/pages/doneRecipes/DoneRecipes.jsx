96c01ff5bf4b146cfabf8c73c0f01311e51d27de
TulioKaaz <tuliocaz.windcoast@gmail.com>
2023-03-12 15:57:46 -0300

    Create: Done Recipes page

--- src/pages/doneRecipes/DoneRecipes.jsx
+++ src/pages/doneRecipes/DoneRecipes.jsx
@@ -1,10 +1,57 @@
 import React from 'react';
-import { Header } from '../../components';
+import { Header, CategoryList, DoneRecipeCard } from '../../components';
 
 function DoneRecipes() {
+  const [categories, setCategories] = useState([]);
+  const [doneRecipes, setDoneRecipes] = useState([]);
+  const [filteredBy, setFilteredBy] = useState('all');
+
+  const handleGetDoneRecipes = () => {
+    const doneRecipesData = JSON.parse(localStorage.getItem('doneRecipes')) || [];
+    setDoneRecipes(doneRecipesData);
+  };
+
+  const handleGetDoneRecipesByCategory = (category) => {
+    const filterTerm = category.replace('s', '');
+    if (filteredBy === filterTerm) {
+      handleGetDoneRecipes();
+      setFilteredBy('all');
+      return;
+    }
+    const doneRecipesData = JSON.parse(localStorage.getItem('doneRecipes')) || [];
+    const filteredByTermRecipes = doneRecipesData.filter(
+      (recipe) => recipe.type === filterTerm,
+    );
+    setDoneRecipes(filteredByTermRecipes);
+    setFilteredBy(filterTerm);
+  };
+
+  useEffect(() => {
+    setCategories([
+      { strCategory: 'meals', categoryTestId: 'filter-by-meal-btn' },
+      { strCategory: 'drinks', categoryTestId: 'filter-by-drink-btn' },
+    ]);
+    handleGetDoneRecipes();
+  }, []);
+
   return (
     <div>
       <Header hTitle="Done Recipes" hSearchDisabled />
+      <CategoryList
+        categoriesList={ categories }
+        handleGetAll={ handleGetDoneRecipes }
+        handleGetByCategory={ handleGetDoneRecipesByCategory }
+        allCategoryTestId="filter-by-all-btn"
+      />
+      <div className="done-recipes">
+        {doneRecipes.map((recipes, index) => (
+          <DoneRecipeCard
+            key={ index }
+            index={ index }
+            recipeData={ recipes }
+          />
+        ))}
+      </div>
     </div>
   );
 }
